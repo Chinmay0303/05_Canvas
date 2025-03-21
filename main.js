f772623c73a91d664a388f0ef195f36a81f6a040
Chinmay0303 <99880235+Chinmay0303@users.noreply.github.com>
2025-03-21 11:27:45 +0530

    create canvas element
    add canvasDiv when clicked on any button
    remove canvasDiv when clicked on cross

--- main.js
+++ main.js
@@ -4,11 +4,39 @@ log('main');
 
 const buttons = document.querySelectorAll('button');
 
+const mainContainer = document.querySelector('.main-container');
+
+const canvasDiv = document.createElement('div');
+canvasDiv.setAttribute('class','canvas-div');
+
+    const crossDiv = document.createElement('div');
+    crossDiv.setAttribute('class','cross-div');
+
+        const crossButton = document.createElement('button');
+        crossButton.setAttribute('class','cross-button');
+        crossButton.textContent = 'x';
+
+    crossDiv.appendChild(crossButton);
+
+canvasDiv.appendChild(crossDiv);
+
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('id','main-canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.height = window.innerHeight/1.5;
+    canvas.width = window.innerWidth/1.75;
+
+
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    canvasDiv.appendChild(canvas);
+
 document.addEventListener('mouseover',mouseOverEvent);
 
 function mouseOverEvent(event){
-    if(event.target.className == 'button'){
-        log(event.target);
+    if(event.target.className == 'button' || event.target.className == 'cross-button'){
         event.target.classList.add('over');
     }
 }
@@ -16,8 +44,21 @@ function mouseOverEvent(event){
 document.addEventListener('mouseout',mouseOutEvent);
 
 function mouseOutEvent(event){
+    if(event.target.className == 'button over' || event.target.className == 'cross-button over'){
+        event.target.classList.remove('over');
+    }
+}
+
+document.addEventListener('click',clickEvent);
+
+function clickEvent(event){
     if(event.target.className == 'button over'){
         log(event.target);
+        mainContainer.appendChild(canvasDiv);
+    }
+    else if(event.target.className == 'cross-button over'){
+        log(event.target);
         event.target.classList.remove('over');
+        mainContainer.removeChild(canvasDiv);
     }
 }
